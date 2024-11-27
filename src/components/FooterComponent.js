39ae07782c2da5c5788f1db827dc953a95585cf0
front-endapps <website_dev@yahoo.com>
2024-11-27 20:55:16 +0200

    header&footer

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import { LitElement, html, css } from "lit";
+
+export class FooterComponent extends LitElement {
+  static styles = css`
+  `;
+
+  render() {
+    return html`
+      
+    
+    `;
+  }
+}
+
+customElements.define("footer-component", FooterComponent);
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,16 @@
+import { LitElement, html, css } from "lit";
+
+
+export class HeaderComponent extends LitElement {
+  static styles = css`
+  `;
+
+ 
+  render() {
+    return html`
+     
+    `;
+  }
+}
+
+customElements.define("header-component", HeaderComponent);
