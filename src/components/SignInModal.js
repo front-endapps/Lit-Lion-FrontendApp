b446a7030bccdc214ab1fb85fa2ae9a253b508e2
front-endapps <website_dev@yahoo.com>
2024-11-28 07:18:41 +0200

    modal sign in

---
+++ src/components/SignInModal.js
@@ -0,0 +1,15 @@
+import { LitElement, html, css } from "lit";
+
+
+export class SignInModal extends LitElement {
+  static styles = css`
+  `;
+
+  render() {
+    return html`
+    `;
+  }
+}
+
+customElements.define("sign-in-modal", SignInModal);
+
